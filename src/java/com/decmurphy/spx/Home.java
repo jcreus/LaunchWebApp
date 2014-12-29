fc998a5e699b4aac647b99795fd8b5ca0e37db23
Declan Murphy <murphd37@tcd.ie>
2014-12-29 00:04:37 +0000

    2014-12-28-03 - develop branch setup

--- src/java/com/decmurphy/spx/Home.java
+++ src/java/com/decmurphy/spx/Home.java
@@ -31,6 +31,11 @@ public class Home extends HttpServlet {
             + "   <title>" + title + "</title>\n"
             + "   <meta charset=\"UTF-8\">\n"
             + "   <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+            
+            + "   <link rel=\"stylesheet\" href=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css\">\n"
+            + "   <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js\"></script>\n"
+            + "   <script src=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js\"></script>\n"
+            
             + "   <link rel=\"stylesheet\" href=\"" + request.getContextPath() + "/css/style.css\" type=\"text/css\"/>\n"
             + "   <link rel=\"stylesheet\" href=\"" + request.getContextPath() + "/css/tabs.css\" type=\"text/css\"/>\n"
             + "   <link rel=\"stylesheet\" href=\"" + request.getContextPath() + "/css/button.css\" type=\"text/css\"/>\n"
