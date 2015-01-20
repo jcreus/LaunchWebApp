c25e5701f9f09c5632ae49f8bbdd95c05d4382ff
dmurphy <dmurphy@cartrawler.com>
2015-01-20 18:00:14 +0000

    2015-01-20-09 - New favicon

--- src/java/com/decmurphy/spx/servlet/Home.java
+++ src/java/com/decmurphy/spx/servlet/Home.java
@@ -43,7 +43,7 @@ public class Home extends HttpServlet {
             + "    <link rel=\"stylesheet\" href=\"css/button.css\" type=\"text/css\"/>\n"
 
 			+ "	   <link rel=\"shortcut icon\" href=\"images/favicon.ico\" type=\"image/x-icon\">\n"
-			+ "    <link rel=\"icon\" href=\"images/favicon.ico\" type=\"image/x-icon\">\n"
+			+ "    <link rel=\"icon\" href=\"images/favicon-logo.png\" type=\"image/x-icon\">\n"
             + "  </head>\n"
             + "  <body>\n"
             + "    <div class=\"bg\">\n"
