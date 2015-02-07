52a58a36349f7a56a6b832317c067dd8323b7b2a
Declan Murphy <murphd37@tcd.ie>
2015-02-07 18:14:09 +0000

    create superdraco

---
+++ src/java/com/decmurphy/spx/engine/SuperDraco.java
@@ -0,0 +1,16 @@
+package com.decmurphy.spx.engine;
+
+import static com.decmurphy.spx.Globals.g0;
+
+/**
+ *
+ * @author declan
+ */
+public class SuperDraco extends Engine {
+
+  public SuperDraco() {
+    setIsp(235, 134);
+    setThrust(128000, 73000);
+    setMdot(getVacThrust() / (getVacIsp() * g0));
+  }
+}
