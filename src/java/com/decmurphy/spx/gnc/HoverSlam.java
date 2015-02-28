08073bac9d0c4b50cbe727ff054dd222a15e73a7
Declan Murphy <murphd37@tcd.ie>
2015-02-28 18:48:07 +0000

    Hoverslam stall guard

--- src/java/com/decmurphy/spx/gnc/HoverSlam.java
+++ src/java/com/decmurphy/spx/gnc/HoverSlam.java
@@ -4,6 +4,8 @@ import static com.decmurphy.spx.gnc.Navigation.gravityTurn;
 import static com.decmurphy.spx.gnc.Navigation.leapfrogStep;
 import com.decmurphy.spx.vehicle.Stage;
 import static com.decmurphy.utils.Globals.dt;
+import com.decmurphy.utils.Maths;
+import static com.decmurphy.utils.Maths.magnitudeOf;
 import static java.lang.Math.abs;
 import static java.lang.Math.max;
 import java.math.RoundingMode;
@@ -30,17 +32,25 @@ public class HoverSlam {
 			minThrottle = max(minThrottle, currentThrottle);
 		}
 
-		if (throttleTest(stage, maxThrottle) > 0.0) {
-			if (throttleTest(stage, minThrottle) < 0.0) {
+		if (throttleTest(stage, maxThrottle) > 0.0) {        // If maxThrottle stops you above ground
+			if (throttleTest(stage, minThrottle) < 0.0) {      // If minThrottle makes you crash
 				do {
-					double testThrottle = 0.5 * (minThrottle + maxThrottle);
-					double endHeight = throttleTest(stage, testThrottle);
+					
+					double testThrottle = 0.5*(minThrottle + maxThrottle);		// Test average of two			
+					double endHeight = throttleTest(stage, testThrottle);     // Find end altitude
+					
 					if (abs(endHeight) < 1) {
 						System.out.println(df.format(stage.getParent().clock()) + ": New throttle: " + testThrottle + " --S: " + stage.alt() + " --V: " + stage.relVel());
 						return testThrottle;
 					}
+					
 					minThrottle = endHeight < 0 ? testThrottle : minThrottle;
 					maxThrottle = endHeight > 0 ? testThrottle : maxThrottle;
+					
+					// Stalls sometimes when fuel runs out. This breaks out of the stall
+					if(abs(minThrottle - maxThrottle) < 1e-3)
+						return minThrottle;
+					
 				} while (true);
 			} else {
 				System.out.println(df.format(stage.getParent().clock()) + ": New throttle: ..0.0" + " --S: " + stage.alt() + " --V: " + stage.relVel());
@@ -59,26 +69,28 @@ public class HoverSlam {
 		copyStage.setThrottleTest(true);
 
 		double S = copyStage.alt();
-		double vel;
+		double[] vel;
 
 		do {
 
 			leapfrogStep(copyStage);
 			gravityTurn(copyStage);
 
-			vel = (copyStage.alt() - S)/dt; // radial velocity
+			vel = new Maths.CartesianVelocity(copyStage.relVel)
+					.convertToSpherical(new Maths.CartesianCoordinates(copyStage.relPos))
+					.getValues();
+			
 			S = copyStage.alt();
+			
+			if(S <= 0) break;                          // If crash, S<0, started too late
+			if(copyStage.getPropMass() < 500.0) break; // If fuel runs out, S>0, started too soon
+			if(magnitudeOf(vel) < 6.0)	break;         // If stopped, S>0, started too soon
 
-		} while (vel <= 0.0 && S >= 0.0 && copyStage.getPropMass() > 500.0);
-		/*
-		if(copyStage.getPropMass() <= 500) {
-			throw new FuelException()
-		}
-*/
+		} while (true);
+
 		copyStage = null;
 		return S;
 
-//		return 0;
 	}
 
 }
